400d8d19958fd4a9475ff9ac9c954ce371d07835
tinnguyen <tinnguyen2922000@gmail.com>
2026-05-11 12:52:45 +0900

    Ls2

--- src/main/java/com/example/ls4_1_author/Model/Author.java
+++ src/main/java/com/example/ls4_1_author/Model/Author.java
@@ -1,5 +1,48 @@
-package com.example.ls4_1_author.Model.Entity;
+package com.example.ls4_1_author.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "authors")
 
 public class Author {
-    
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(columnDefinition = "varchar(100)", nullable = false)
+    private String name;
+
+  private String email;
+
+    public Author(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    public Author() {};
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
---
+++ src/main/java/com/example/ls4_1_author/Model/Book.java
@@ -0,0 +1,60 @@
+package com.example.ls4_1_author.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String title;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, double price, Author author) {
+        Id = id;
+        this.title = title;
+        this.price = price;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
